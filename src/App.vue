003ff219859fee6d5cf3ba5ee2f6998a3875decf
haruki <kinikun@outlook.com>
2025-06-11 20:49:10 +0800

    pinia持久化

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { useTestStore } from './stores'
 
+const testStore = useTestStore()
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    {{ testStore.testVal }}
+    <img @click="testStore.testValAdd()" alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
---
+++ src/stores/test.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useTestStore = defineStore(
+  'tweblog-public-test',
+  () => {
+    const testVal = ref(0)
+
+    const testValAdd = () => {
+      testVal.value += 1
+    }
+
+    return {
+      testVal,
+      testValAdd,
+    }
+  },
+  {
+    persist: true, // 持久化
+  },
+)
